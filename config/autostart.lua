b9c56918fb47e7302276408a508f84797576bf1f
Rohith raj <120798891+INFERNORAJ12@users.noreply.github.com>
2026-09-06 19:14:41 +0530

    Update autostart.lua

--- config/autostart.lua
+++ config/autostart.lua
@@ -5,8 +5,8 @@ local wallpaper_id = dofile("/home/kannan/.config/hypr/config/wallpaper.lua")
 hl.on("hyprland.start", function ()
     hl.exec_cmd("dbus-update-activation-environment --systemd --all")
     hl.exec_cmd("gnome-keyring-daemon --start --components=secrets")
-    hl.exec_cmd("fcitx5 -d")
+    --hl.exec_cmd("fcitx5 -d") #use this for japanese keyboard
     hl.exec_cmd("noctalia")
     hl.exec_cmd("xhost +SI:localuser:root")
-    hl.exec_cmd("linux-wallpaperengine --screen-root HDMI-A-1 --silent " .. wallpaper_id)
-end)
+    hl.exec_cmd("linux-wallpaperengine --screen-root HDMI-A-1 --silent " .. wallpaper_id)--very important for wallpaper engine to work on ur pc from boot up can the aruguments according to your need this is an example for my pc
+end) 
